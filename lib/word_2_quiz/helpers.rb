a3133f2e743e07f583f3bb0f99abbda21a3eb472
David Spencer <davidjspencer@ufl.edu>
2016-12-22 21:10:15 -0700

    Refactor to put make the quiz class consistent with the other classes

--- lib/word_2_quiz/helpers.rb
+++ lib/word_2_quiz/helpers.rb
@@ -1,13 +1,19 @@
 require "numbers_in_words"
 module Word2Quiz
   module Helpers
+    TIME_TO_MINUTES_MAP = {
+      "hours" => 60,
+      "minutes" => 1,
+    }.freeze
+
     ##
-    # Takes in an array of indexes, and spits out an array of arrays of lines
-    # bounded by the indexes.
+    # Takes in an array of indexes, and spits out an array of arrays of
+    # paragraphs bounded by the indexes.
+    ##
     def self.map_to_boundaries(indexes:, paragraphs:)
       indexes.map.with_index do |start_index, i|
         first_index = start_index
-        last_index = indexes[i+1] || paragraphs.count
+        last_index = indexes[i + 1] || paragraphs.count
 
         paragraphs[first_index...last_index]
       end
@@ -15,26 +21,56 @@ module Word2Quiz
 
     def self.get_quiz_duration(paragraphs)
       time_paragraph = paragraphs.find do |p|
-        p.text.include?("This examination consists of one session not to exceed ")
+        p.text.include?("one session not to exceed ")
       end
 
       time = time_paragraph.text.sub(
         "This examination consists of one session not to exceed ",
-        ""
+        "",
       ).chomp(".")
 
       time_number = time.split(" ")[0]
       time_unit = time.split(" ")[1]
       unit_conversion = TIME_TO_MINUTES_MAP[time_unit]
-      quiz_duration = NumbersInWords.in_numbers(time_number) * unit_conversion
+
+      NumbersInWords.in_numbers(time_number) * unit_conversion
     end
 
+    ##
+    # Returns the quiz title. The 3rd and 4th lines contain the best title for
+    # the quiz.
+    ##
     def self.get_quiz_title(paragraphs)
       paragraphs[3..4].map(&:text).join(" ")
     end
 
+    ##
+    # Returns the quiz description. Lines 0-5 are the best description for the
+    # quiz.
+    ##
     def self.get_quiz_description(paragraphs)
       paragraphs[0..5].map(&:to_html).join("\n")
+    end
+
+    ##
+    # Finds the index of the paragraph where the questions start
+    ##
+    def self.get_question_start_index(paragraphs)
+      paragraphs.find_index { |p| p.text.include?("Multiple Choice") }
+    end
+
+    ##
+    # Finds the index of the paragraph where the questions end
+    ##
+    def self.get_question_end_index(paragraphs)
+      paragraphs.find_index { |p| p.text.include?("End of examination") }
+    end
+
+    ##
+    # Returns the indexes of the start of each question.
+    ##
+    def self.get_question_indexes(paragraphs)
+      paragraphs.each_index.select { |i| paragraphs[i].text.match(/^\d*\./) }
     end
   end
 end
