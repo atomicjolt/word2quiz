a33c0584b4e2439c67b81f753d34ead6e99ab0f2
David Spencer <davidjspencer@ufl.edu>
2016-12-23 16:35:23 -0700

    Update the solution parser to correctly get only matched text

--- lib/word_2_quiz/quiz_solutions_parser.rb
+++ lib/word_2_quiz/quiz_solutions_parser.rb
@@ -1,24 +1,21 @@
 require "yomu"
 require "docx"
 require "strscan"
-require "word_2_quiz/quiz"
-require "word_2_quiz/helpers"
-require "byebug"
+
 module Word2Quiz
   def self.parse_answers(file_path)
     yomu = Yomu.new file_path
     text = yomu.text
+
     scanner = StringScanner.new(text)
     answers = {}
-    while !scanner.eos? do
-      question_answer = scanner.scan(/\d+\.\s\s[A-Z]/)
-      byebug
-      answer_data = question_answer.split(".")
-      # First is the question number, Second is the letter answer
-      answers[answer_data[0]] = answer_data[1]
+
+    while scanner.scan_until(/\d+\.\s+[A-Z]/) && !scanner.eos?
+      solution_text = scanner.matched.split(".").map(&:strip)
+      question_answer = [solution_text].to_h
+      answers.merge!(question_answer)
     end
-    byebug
+
     answers
   end
-
 end
