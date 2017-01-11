bdbf5e254e6962f06099b59a31b3b4f0872f1f33
dspencer001 <dspencer577@gmail.com>
2017-01-10 17:57:03 -0700

    Require the solution parser

--- lib/word_2_quiz/quiz_parser.rb
+++ lib/word_2_quiz/quiz_parser.rb
@@ -1,6 +1,7 @@
 require "docx"
 require "word_2_quiz/quiz"
 require "word_2_quiz/helpers"
+require "word_2_quiz/quiz_solutions_parser"
 
 module Word2Quiz
   def self.parse_quiz(quiz_path, answer_key_path)
